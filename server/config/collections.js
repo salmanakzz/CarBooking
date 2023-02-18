299f27339b5a7e307b640173306d950200be8048
salmanakzz <salmanakzz808@gmail.com>
2023-02-18 17:31:08 +0530

    initial commit

---
+++ server/config/collections.js
@@ -0,0 +1,5 @@
+// all collections of database are declared here
+
+module.exports = {
+  CAR_COLLECTION: "car",
+}
---
+++ server/config/models/carModel.js
@@ -0,0 +1,22 @@
+// car collection shema
+
+const mongoose = require("mongoose");
+const { CAR_COLLECTION } = require("../collections");
+
+const car = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    brand: { type: String, required: true },
+    segment: { type: String, required: true },
+    location: { type: String, required: true },
+    booked: { type: Array },
+  },
+  {
+    timestamps: true,
+    collection: CAR_COLLECTION,
+  }
+);
+
+const model = mongoose.model("carData", car);
+
+module.exports = model;
